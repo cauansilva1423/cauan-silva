13aec68444d531ea0dd7d9019460bede312f7786
Cauan Silva <cauansilva142306@gmail.com>
2026-08-18 18:42:52 -0300

    adicionar4

--- atividades/src/atv_04.java
+++ atividades/src/atv_04.java
@@ -4,11 +4,27 @@ public class atv_04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int vetor[] = {53,9,15,62,3};
-        //acharNumeros
+
 
     }
-    public static void acharNumeros(){
-        
+    public static void MaiorElemento(int vetor[]){
+        int maior = vetor[0];
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] > maior) {
+                maior = vetor[i];
+            }
+        }
+    }
+    public static void MenorValor(int vetor[]){
+        int menor = vetor[0];
+        for (int i= 0; i < vetor.length; i++){
+            if (vetor[i]< menor){
+                menor = vetor[i];
+            }
+        }
+    }
+    public static void MediaValor(int vetor[]){
+
     }
 }
 
